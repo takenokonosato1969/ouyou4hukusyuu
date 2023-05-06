a74bcabf600e05dcdb165756a086f419d40710a6
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-05-06 06:01:24 +0000

    応用課題4復習

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index,:show,:edit,:update]
+
+end
