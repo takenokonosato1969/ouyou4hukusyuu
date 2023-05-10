bf0837d5f928069c4b28bd310538d8c1a535c179
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-05-10 02:57:33 +0000

    ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
-  
+
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォローしている人の取得
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォローされている人の取得
-  
+
   # ここではforeign_keyで繋いだところの定義をおこなう。
   has_many :following_user, through: :follower, source: :followed #自分がフォロー
   # following_user:中間テーブルを通し、followedモデルのフォローされる側を取得
@@ -20,27 +20,27 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   #ユーザーをフォロー
   def follow(user_id)
     follower.create(followed_id: user_id)
   end
-  
+
   #フォローを外す
   def unfollow(user_id)
     follower.find_by(followed_id: user_id).destroy
   end
-  
+
   #フォローしてればtrueを返す
   def following?(user)
     following_user.include?(user)
   end
-  
+
 
   def self.search_for(content,method)
     if method == "perfect"
@@ -53,6 +53,13 @@ class User < ApplicationRecord
         User.where("name LIKE ?", "%" + content + "%")
     end
   end
-  
-  
+
+  def self.guest
+    find_or_create_by!(name: "guestuser" ,email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+
 end
